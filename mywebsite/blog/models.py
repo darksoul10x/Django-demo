9b041c0fdf3a2b500b5585361a7eff4317ed7246
giteverything01 <143570934+giteverything01@users.noreply.github.com>
2023-09-15 09:52:56 +0700

    update UI

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     Title = models.CharField(max_length=200)
-    Author_Name = models.CharField(max_length=300)
-    Content = models.CharField(max_length=1000)
-    URL = models.URLField()
+    Content = models.TextField()
+    Img = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.Title
 
     class Meta:
         db_table = 'Blog'
